85d4f8bccc73b082cf1c6aadf331d59087b01e8e
Kris <krisziel@mac.com>
2015-03-20 01:30:53 -0700

    styling and ajax calls

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var base = 'http://localhost:3000/'
+
 var Flight = Backbone.Model.extend({
   initialize:function(){
     this.on("change:equipment",function(model){
@@ -59,3 +61,53 @@ var flightTwo = new Flight({ airline:'Southworst', flightNumber:'001', equipment
 var flightList = new FlightList([flightOne, flightTwo]);
 
 new FlightListView({el:'#flightsList'});
+
+$.getJSON(base + 'airport').done(function(data){
+	var airports = data;
+	var airportModel = new Airport(airports);
+	console.log(airportModel);
+});
+
+
+var Airport = Backbone.Model.extend({
+	initialize:function(){
+		
+	}
+});
+
+var airportList = Backbone.Collection.extend({
+	model:Airport
+});
+
+var AirportView = Backbone.View.extend({
+  initialize:function(){
+    this.render();
+    this.listenTo(this.model, 'change', this.render);
+  },
+  /*events:{
+    'click .edit':'editFlight',
+    'click .save':'saveFlight'
+  },*/
+  render:function(){
+    var variables = this.model.attributes;
+    var template = _.template($('#airportTemplate').html(),variables);
+    this.$el.html(template);
+    return this;
+  },
+});
+
+var AirportListView = Backbone.View.extend({
+  initialize:function(){
+    this.render();
+    this.listenTo(flightList, 'add', this.addOne);
+  },
+  render:function(){
+    this.addAll();
+  },
+  addAll:function(){
+    this.$el.html('');
+    flightList.each(this.addOne,this);
+  }
+});
+
+new AirportListView({el:'#airportList'});
