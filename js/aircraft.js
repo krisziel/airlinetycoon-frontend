f2454d9d609db7bf3f278fd0b0c862301e7a1ae9
Kris <krisziel@mac.com>
2015-03-29 00:48:33 -0700

    inline config for aircraft purchase

--- js/aircraft.js
+++ js/aircraft.js
@@ -88,11 +88,31 @@ var AircraftView = Backbone.View.extend({
 	loadAircraftPurchase:function(){
 		selectedAircraft = this.model;
 		var variables = this.model.attributes;
+		variables.maxFlights = changeAircraftQuantity(1000).quantity;
+		if((variables.iata.match(/CR2|ERJ/gi))) {
+			variables.config = getConfigSpecs({f:{p:0.0,i:6},j:{p:0.0,i:3},p:{p:0.0,i:1},y:{p:1.0,i:0}});
+		} else if(variables.iata.match(/73|31|32[01]/gi)) {
+			variables.config = getConfigSpecs({f:{p:0.0,i:7},j:{p:0.1,i:4},p:{p:0.2,i:1},y:{p:0.7,i:0}});
+		} else {
+			variables.config = getConfigSpecs({f:{p:0.12,i:8},j:{p:0.30,i:5},p:{p:0.14,i:2},y:{p:0.44,i:0}});
+		}
+		this.model.attributes = variables;
 		var template = _.template($('#aircraftPurchaseTemplate').html(),variables);
 		$('body').append(template);
+		$('.row.config-row[data-rowtype="price"] .price').popup();
+		$('.selection.dropdown').dropdown();
 		$('.row.config-row[data-rowtype="quantity"] input[type="range"]').on('input change',function(){
-			console.log(changeAircraftQuantity($(this).val()));
+			var quantity = changeAircraftQuantity($(this).val());
+			$('.row.config-row[data-rowtype="quantity"] span').html(quantity.quantity);
+			$('.row.config-row[data-rowtype="price"] .price').html('$' + comma(quantity.price) + ' ($' + comma(quantity.price*quantity.quantity) + ' total)');
 		});
+		$('.selection.dropdown[data-rowtype="configuration"] input').on('change',function(){
+			var id = $(this).val();
+			fillConfigDetails(id);
+		});
+		if(variables.user.configs.length > 0) {
+			fillConfigDetails(variables.user.configs[0].id);
+		}
 		$('#aircraftPanel').modal('show');
 	},
 	loadAircraftConfigs:function(){
@@ -169,6 +189,29 @@ var UserAircraftListView = Backbone.View.extend({
 	}
 });
 
+function fillConfigDetails(id) {
+	var config = JSON.parse($('[data-rowtype="configuration"] .item[data-value="' + id + '"]').data('json').replace(/\'/gi,"\""));
+	$('.row.config-row[data-cabintype="f"] input[type="range"]').val(config.f.count).on('change input',function(){ changeConfiguration('f'); });
+	$('.row.config-row[data-cabintype="j"] input[type="range"]').val(config.j.count).on('change input',function(){ changeConfiguration('j'); });
+	$('.row.config-row[data-cabintype="p"] input[type="range"]').val(config.p.count).on('change input',function(){ changeConfiguration('p'); });
+	$('.row.config-row[data-cabintype="y"] input[type="range"]').val(config.y.count).on('change input',function(){ changeConfiguration('y'); });
+	_.each(['f','j','p','y'],function(cabin){ changeConfiguration(cabin); });
+}
+function getConfigSpecs(config) {
+	var sqft = selectedAircraft.attributes.sqft;
+	var configData = {def:{f:0,j:0,p:0,y:0},max:{f:0,j:0,p:0,y:0},layout:config};
+	var sqftLeft = sqft;
+	_.each(config,function(value,cabin){
+		configData.max[cabin] = Math.floor(sqft/seats[value.i].sqft);
+		if(cabin === 'y') {
+			configData.def['y'] = Math.floor(sqftLeft/seats[value.i].sqft);
+		} else {
+			configData.def[cabin] = Math.floor((sqft*value.p)/seats[value.i].sqft);
+			sqftLeft -= (configData.def[cabin]*seats[value.i].sqft);
+		}
+	});
+	return configData;
+}
 function stringifyConfig(config) {
 	var classes = [];
 	config.f.count > 0 ? classes.push('F: ' + config.f.count) : '';
@@ -179,11 +222,14 @@ function stringifyConfig(config) {
 }
 function changeConfiguration(cabinClass) {
   var aircraft = selectedAircraft;
-  var availableSpace = aircraft.sqft;
-  var classType = {f:{name:"First",id:9},j:{name:"Business",id:6},p:{name:"Premium",id:2},y:{name:"Economy",id:1}};
-  $.each(classType,function(key,value){
-    var seat = seats[value.id];
-    var row = $('.row[data-cabintype="' + key + '"]');
+  var availableSpace = selectedAircraft.attributes.sqft;
+  _.each(selectedAircraft.attributes.config.layout,function(seat,cabin){
+		console.log(availableSpace);
+    var seat = seats[seat.i];
+		if(cabin === 'f') {
+			console.log(seat);
+		}
+    var row = $('.row[data-cabintype="' + cabin + '"]');
     var seatsVal = Math.max(Math.ceil(row.find('input[type="range"]').val()),0);
     var space = (seatsVal*seat.sqft);
     if(space > availableSpace) {
@@ -219,13 +265,12 @@ function setLayout() {
 	});
 }
 function changeAircraftQuantity(quantity) {
-	console.log(quantity);
 	var selection = selectedAircraft.attributes;
-  var maxPlanes = Math.floor(airline.money/selection.price);
+  var discount = Math.min(((quantity-1)*selection.discount),50);
+  var price = Math.round(selection.price*(1-(discount*0.01)));
+  var maxPlanes = Math.floor(airline.money/price);
   if(quantity > maxPlanes) {
     quantity = maxPlanes;
   }
-  var discount = Math.min(((quantity-1)*selection.discount),50);
-  var price = Math.round(selection.price*(1-(discount*0.01)));
   return {quantity:quantity,price:price,discount:discount};
 }
