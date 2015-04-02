0421edc284d6b1dc8051c03bffb4fe23de80bcb6
krisziel <krisziel@mac.com>
2015-04-01 17:51:19 -0700

    unstyled flights list

--- js/flight.js
+++ js/flight.js
@@ -3,12 +3,13 @@ var flightList = [];
 function loadFlights() {
 	$.getJSON(base + 'flight' + cookies.url).done(function(data){
 		_.each(data,function(flight){
-			flightList.push(new Flight(flight));
+			var newFlight = new Flight(flight);
+			flightList.push(newFlight);
 			var route = [flight.route.origin.id,flight.route.destination.id].sort();
 			routeList[route] = flight.route;
 		});
 		flightList = new FlightList(flightList);
-		new FlightListView({el:'#routeList'});
+		new FlightListView({el:'#flightList'});
 	});
 }
 
@@ -25,7 +26,9 @@ var FlightView = Backbone.View.extend({
   },
   render:function(){
     var variables = this.model.attributes;
+		console.log(variables);
     var template = _.template($('#flightTemplate').html(),variables);
+		console.log(template);
     this.$el.html(template);
     return this;
   },
@@ -40,19 +43,20 @@ var FlightView = Backbone.View.extend({
 var FlightListView = Backbone.View.extend({
   initialize:function(){
     this.render();
-    this.listenTo(airportList, 'add', this.addOne);
+    this.listenTo(flightList, 'add', this.addOne);
   },
   render:function(){
     this.addAll();
   },
   addOne:function(flight){
+		console.log(flight);
     var view = new FlightView({model: flight});
     this.$el.append(view.$el);
   },
   addAll:function(){
 		var template = _.template($('#flightListTemplate').html());
     this.$el.html(template);
-    airportList.each(this.addOne,this);
+    flightList.each(this.addOne,this);
   }
 });
 
@@ -68,4 +72,18 @@ function minutesToHours(minutes) {
   var hours = Math.floor(minutes/60);
   var minutes = (minutes%60);
   return hours + ":" + minutes;
+}
+function maxFlights(route, aircraft) {
+	var hours = (route.distance/aircraft.speend)
+	var minutes = hours*60;
+	var totalFlight = (minutes+turn_time);
+	var frequencies = (10080(totalFlight*2));
+	return frequencies;
+}
+
+var cabinType = {
+	f:"first",
+	j:"business",
+	p:"premium economy",
+	y:"economy"
 }
