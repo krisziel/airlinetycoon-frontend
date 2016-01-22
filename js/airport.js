3a6094a333b1e632c59baafb9f196478e7bd9ed7
krisziel <krisziel@mac.com>
2016-01-22 20:59:11 +1100

    beginning to fill airport info modal with dynamic data

--- js/airport.js
+++ js/airport.js
@@ -84,8 +84,61 @@ function loadAirports(){
 	});
 }
 function loadAirport(id) {
-  var airportData = airportList.get(id)
-  var template = _.template($('#airportInfoModalTemplate').html(), airportData);
-  $('body').append(template);
-	$('#airportPanel').modal('show');
+	$.getJSON(base + 'airport/' + id + cookies.url).done(function(data){
+    var template = _.template($('#airportInfoModalTemplate').html(), data);
+    $('body').append(template);
+  	$('#airportPanel').modal('show');
+	});
+}
+function loadAirportAirlines() {
+  var ctx = $("#chart1").get(0).getContext("2d");
+  var data = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    }
+  ]
+  var myPieChart = new Chart(ctx).Pie(data,{});
+}
+function loadAirportDestinations() {
+  var ctx = $("#chart2").get(0).getContext("2d");
+  var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.5)",
+            strokeColor: "rgba(151,187,205,0.8)",
+            highlightFill: "rgba(151,187,205,0.75)",
+            highlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+  };
+  var myBarChart = new Chart(ctx).Bar(data, {});
+}
+function loadAirportFlights() {
+
 }
