b75dce50b04845f5d3df1e70262bceed2da97980
krisziel <krisziel@mac.com>
2016-02-04 23:36:52 -0800

    added table for airport market share data

--- js/airport.js
+++ js/airport.js
@@ -88,104 +88,12 @@ function loadAirport(id) {
     var template = _.template($('#airportInfoModalTemplate').html(), data);
     $('body').append(template);
   	$('#airportPanel').modal('show');
-    var colors = [
-      {
-        color:"#F7464A",
-        highlight: "#FF5A5E"
-      },
-      {
-        color: "#46BFBD",
-        highlight: "#5AD3D1"
-      },
-      {
-        color: "#FDB45C",
-        highlight: "#FFC870"
-      },
-      {
-        color:"#F7464A",
-        highlight: "#FF5A5E"
-      },
-      {
-        color: "#46BFBD",
-        highlight: "#5AD3D1"
-      },
-      {
-        color: "#FDB45C",
-        highlight: "#FFC870"
-      },
-      {
-        color:"#F7464A",
-        highlight: "#FF5A5E"
-      },
-      {
-        color: "#46BFBD",
-        highlight: "#5AD3D1"
-      },
-      {
-        color: "#FDB45C",
-        highlight: "#FFC870"
-      },
-      {
-        color: "#46BFBD",
-        highlight: "#5AD3D1"
-      },
-      {
-        color: "#FDB45C",
-        highlight: "#FFC870"
-      },
-      {
-        color:"#F7464A",
-        highlight: "#FF5A5E"
-      },
-      {
-        color: "#46BFBD",
-        highlight: "#5AD3D1"
-      },
-      {
-        color: "#FDB45C",
-        highlight: "#FFC870"
-      },
-      {
-        color:"#F7464A",
-        highlight: "#FF5A5E"
-      },
-      {
-        color: "#46BFBD",
-        highlight: "#5AD3D1"
-      },
-      {
-        color: "#FDB45C",
-        highlight: "#FFC870"
-      }
-    ]
-    graphAirportDestinations(data.marketShares.airlines);
-    graphAirportFlights(data.marketShares, colors);
-    graphAirportPassengers(data.marketShares, colors);
+    $('table#airlineMarketShareTable').tablesort().data('tablesort').sort($("th.default-sort"));
+    var colors = [{color:"#058DC7",highlight: "#FF5A5E"},{color: "#50B432",highlight: "#5AD3D1"},{color: "#ED561B",highlight: "#FFC870"},{color:"#DDDF00",highlight: "#FF5A5E"},{color: "#24CBE5",highlight: "#5AD3D1"},{color: "#64E572",highlight: "#FFC870"},{color:"#FF9655",highlight: "#FF5A5E"},{color: "#FFF263",highlight: "#5AD3D1"},{color: "#6AF9C4",highlight: "#FFC870"},{color: "#46BFBD",highlight: "#5AD3D1"},{color: "#FDB45C",highlight: "#FFC870"},{color:"#F7464A",highlight: "#FF5A5E"},{color: "#46BFBD",highlight: "#5AD3D1"},{color: "#FDB45C",highlight: "#FFC870"},{color:"#F7464A",highlight: "#FF5A5E"},{color: "#46BFBD",highlight: "#5AD3D1"},{color: "#FDB45C",highlight: "#FFC870"}];
+    // graphAirportDestinations(data.marketShares.airlines);
+    // graphAirportFlights(data.marketShares, colors);
+    // graphAirportPassengers(data.marketShares, colors);
 	});
-}
-function loadAirportAirlines() {
-  var ctx = $("#chart1").get(0).getContext("2d");
-  var data = [
-    {
-        value: 300,
-        color:"#F7464A",
-        highlight: "#FF5A5E",
-        label: "Red"
-    },
-    {
-        value: 50,
-        color: "#46BFBD",
-        highlight: "#5AD3D1",
-        label: "Green"
-    },
-    {
-        value: 100,
-        color: "#FDB45C",
-        highlight: "#FFC870",
-        label: "Yellow"
-    }
-  ]
-  var myPieChart = new Chart(ctx).Pie(data,{});
 }
 function graphAirportDestinations(airlines, graphColors) {
   var airlineNames = [];
@@ -226,7 +134,7 @@ function graphAirportFlights(airport, graphColors) {
     if(i <= 10) {
       var airlineData = {
         value: airline.flights,
-        label: airline.airline.icao,
+        label: airline.airline.name,
         highlight: graphColors[i].highlight,
         color: graphColors[i].color
       };
@@ -260,7 +168,7 @@ function graphAirportPassengers(airport, graphColors) {
     if(i <= 10) {
       var airlineData = {
         value: airline.passengers,
-        label: airline.airline.icao,
+        label: airline.airline.name,
         highlight: graphColors[i].highlight,
         color: graphColors[i].color
       };
